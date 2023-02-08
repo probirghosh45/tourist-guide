362cf57c48d009a9164e31da9be965dc318af113
probir ghosh <probirghosh@outlook.com>
2023-02-08 21:34:24 +0600

    Contact US Page Mail Sending Feature Implimented

--- client/src/Components/ContactUs/Contact.js
+++ client/src/Components/ContactUs/Contact.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { Col, Container, Row } from "react-bootstrap";
 import "./Contact.css";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-
+import MessengerCustomerChat from "react-messenger-customer-chat";
+import Swal from 'sweetalert2';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dzz98ef', 'template_38truc8', form.current, 'rGxrrhDcrhglFcINx')
+      .then((result) => {
+        if(result.text==="OK"){
+          Swal.fire(
+            'Good job!',
+            'Message Sent Successfully 🎉',
+            'success'
+          )
+          form.reset();
+        }
+          // console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+
+  };
   return (
     <>
       <section className="contact">
@@ -39,67 +62,67 @@ const Contact = () => {
                           </div>
                         </div>
 
-                        <div className="form p-3">
-                          <div className="form-row my-5">
-                            <div className="col-lg-6">
+                        <form ref={form} onSubmit={sendEmail}>
+                          <div className="form p-3">
+                            <div className="form-row my-5">
+                              <div className="col-lg-6">
+                                <input
+                                  type="text"
+                                  className="effect-1"
+                                  placeholder="Your Name"
+                                  name="user_name" 
+                                  required="required"
+                                />
+                                <span className="Focus-border"></span>
+                              </div>
+                              <div className="col-lg-6">
                               <input
-                                type="text"
-                                className="effect-1"
-                                placeholder="First Name"
-                              />
-                              <span className="Focus-border"></span>
+                                  type="email"
+                                  className="effect-1"
+                                  name="user_email"
+                                  placeholder="Email Address"
+                                  required="required"
+                                />
+                                <span className="Focus-border"></span>
+                              </div>
                             </div>
-                            <div className="col-lg-6">
-                              <input
-                                type="text"
-                                className="effect-1"
-                                placeholder="Last Name"
-                              />
-                              <span className="Focus-border"></span>
-                            </div>
-                          </div>
 
-                          <div className="form-row">
-                            <div className="col-lg-6">
-                              <input
-                                type="text"
-                                className="effect-1"
-                                placeholder="Email Address"
-                              />
-                              <span className="Focus-border"></span>
-                            </div>
-                            <div className="col-lg-6">
-                              <input
-                                type="text"
-                                className="effect-1"
-                                placeholder="Phone Number"
-                              />
-                              <span className="Focus-border"></span>
-                            </div>
-                          </div>
 
-                          <div className="form-row pt-5">
-                            <div className="col-lg-12">
-                              <input
-                                type="text"
-                                className="effect-1"
-                                placeholder="Type Your Message"
-                              />
-                              <span className="Focus-border"></span>
-                            </div>
-                          </div>
-                          <div className="form-row pt-5">
+
+                            <div className="form-row">
                             <div className="col-lg-6">
-                              <p>
-                                {" "}
-                                <input type="checkbox" /> I'm not a Robot{" "}
-                              </p>
+                                <input
+                                  type="number"
+                                  className="effect-1"
+                                  placeholder="Phone Number"
+                                  name="user_phone_number" 
+                                />
+                                <span className="Focus-border"></span>
+                              </div>
+                              <div className="col-lg-6">
+                                <input
+                                  type="text"
+                                  className="effect-1"
+                                  name="message" 
+                                  placeholder="Type Your Message"
+                                />
+                                <span className="Focus-border"></span>
+                              </div>
+
                             </div>
-                            <div className="col-lg-4">
-                              <button className="btn11">Send Message</button>
+                            <div className="form-row pt-5">
+                              <div className="col-lg-6">
+                                <p>
+                                  {" "}
+                                  <input type="checkbox" /> I'm not a Robot{" "}
+                                </p>
+                              </div>
+                              <div className="col-lg-4">
+                                <button className="btn11" type="submit" >Send Message</button>
+                              </div>
                             </div>
                           </div>
-                        </div>
+                        </form>
                       </div>
                     </div>
                   </div>
